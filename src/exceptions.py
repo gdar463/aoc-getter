4f8d606034ee238bcb4895cfc080856e03520265
gdar463 <63969957+gdar463@users.noreply.github.com>
2024-01-02 17:46:54 +0100

    Changed formatting of exceptions' errors

--- src/exceptions.py
+++ src/exceptions.py
@@ -12,35 +12,36 @@ class Inputs(enum.StrEnum):
 
 class InvalidInputException:
     def __init__(self, item: Inputs, error: str):
-        message = Fore.LIGHTRED_EX + "The input " + Fore.LIGHTBLUE_EX + "\"" + error + "\"" + Fore.LIGHTRED_EX + " is invalid for " + Fore.LIGHTYELLOW_EX + str(item.value) + Fore.RESET
+        message = Fore.LIGHTRED_EX + "\nThe input " + Fore.LIGHTBLUE_EX + "\"" + error + "\"" + Fore.LIGHTRED_EX + " is invalid for " + Fore.LIGHTYELLOW_EX + str(item.value) + Fore.RESET
         sys.stderr.write(message)
 
 
 class InputNotFormattedCorrectlyException:
     def __init__(self, item: Inputs, error: str):
-        message = Fore.LIGHTRED_EX + "The input " + Fore.LIGHTBLUE_EX + "\"" + error + "\"" + Fore.LIGHTRED_EX + " isn't formatted correctly for " + Fore.LIGHTYELLOW_EX + str(item.value) + Fore.RESET
+        message = Fore.LIGHTRED_EX + "\nThe input " + Fore.LIGHTBLUE_EX + "\"" + error + "\"" + Fore.LIGHTRED_EX + " isn't formatted correctly for " + Fore.LIGHTYELLOW_EX + str(item.value) + Fore.RESET
         sys.stderr.write(message)
 
 
 class InvalidSessionTokenException:
     def __init__(self):
-        message = Fore.LIGHTRED_EX + "The session token in the enviromental variable (or that you inputed) is invalid. " + Fore.LIGHTBLUE_EX + "\"AOC_API\"" + Fore.LIGHTRED_EX + " enviromental variable'll be reset" + Fore.RESET
+        message = Fore.LIGHTRED_EX + "\nThe session token in the enviromental variable (or that you inputed) is invalid. " + Fore.LIGHTBLUE_EX + "\"AOC_API\"" + Fore.LIGHTRED_EX + " enviromental variable'll be reset" + Fore.RESET
         os.environ["AOC_TOKEN"] = ""
         sys.stderr.write(message)
 
 
 class ServerErrorException:
     def __init__(self, status_code: int, body: str):
-        message = Fore.LIGHTRED_EX + "The server responded with code " + Fore.LIGHTBLUE_EX + str(status_code) + Fore.LIGHTRED_EX + " and body of: \n" + Fore.LIGHTWHITE_EX + body + Fore.LIGHTRED_EX + "\nTry again in " + Fore.LIGHTCYAN_EX + "5-10" + Fore.LIGHTRED_EX + " minutes or check if the website is down" + Fore.RESET
+        message = Fore.LIGHTRED_EX + "\nThe server responded with code " + Fore.LIGHTBLUE_EX + str(status_code) + Fore.LIGHTRED_EX + " and body of: \n" + Fore.LIGHTWHITE_EX + body + Fore.LIGHTRED_EX + "\nTry again in " + Fore.LIGHTCYAN_EX + "5-10" + Fore.LIGHTRED_EX + " minutes or check if the website is down" + Fore.RESET
         sys.stderr.write(message)
 
 
 class InputNotFoundException:
-    def __init__(self, body: str):
-        message = Fore.LIGHTRED_EX + "The server responded with code " + Fore.LIGHTBLUE_EX + "404" + Fore.LIGHTRED_EX + ". Probably you're using a custom API website, if so be sure to include " + Fore.LIGHTCYAN_EX + "https://" + Fore.LIGHTRED_EX + " or " + Fore.LIGHTCYAN_EX + "http://" + Fore.LIGHTRED_EX + " and to not put the trailing slash. \nAlso be sure that the website uses the format " + Fore.LIGHTCYAN_EX + "\"" + Fore.LIGHTYELLOW_EX + "<API Website>" + Fore.LIGHTCYAN_EX + "/" + Fore.LIGHTYELLOW_EX + "<year>" + Fore.LIGHTCYAN_EX + "/day/" + Fore.LIGHTYELLOW_EX + "<day>" + Fore.LIGHTCYAN_EX + "/input\"" + Fore.LIGHTRED_EX + ". The response body is: \n" + Fore.LIGHTWHITE_EX + body + Fore.RESET
+    def __init__(self, body: str, url: str):
+        message = Fore.LIGHTRED_EX + "\nThe server responded with code " + Fore.LIGHTBLUE_EX + "404" + Fore.LIGHTRED_EX + ". Probably you're using a custom API website, if so be sure to include " + Fore.LIGHTCYAN_EX + "https://" + Fore.LIGHTRED_EX + " or " + Fore.LIGHTCYAN_EX + "http://" + Fore.LIGHTRED_EX + " and to not put the trailing slash. \nAlso be sure that the website uses the format " + Fore.LIGHTCYAN_EX + "\"" + Fore.LIGHTYELLOW_EX + "<API Website>" + Fore.LIGHTCYAN_EX + "/" + Fore.LIGHTYELLOW_EX + "<year>" + Fore.LIGHTCYAN_EX + "/day/" + Fore.LIGHTYELLOW_EX + "<day>" + Fore.LIGHTCYAN_EX + "/input\"" + Fore.LIGHTRED_EX + ". The response body is: \n\n" + Fore.LIGHTWHITE_EX + body + Fore.LIGHTRED_EX + "\nThe request was made to the url: " + Fore.LIGHTCYAN_EX + "\"" + url + "\""
         sys.stderr.write(message)
 
 
+# This function exists only for using colorama
 def custom_raise(exception, *args):
     exception(*args)
     sys.exit()
