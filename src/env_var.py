126de6d257a55653a1fb3e0fabc63c533aa6d5fb
gdar463 <63969957+gdar463@users.noreply.github.com>
2024-01-02 17:48:04 +0100

    Changed to using python-dotenv instead of pure os.environ

--- src/env_var.py
+++ src/env_var.py
@@ -1,14 +1,17 @@
 import os
-from getpass import getpass
+from pwinput import pwinput
 from pathlib import Path
+from colorama import Fore
+from dotenv import load_dotenv, set_key, find_dotenv
+
+load_dotenv()
 
 
 def get_token() -> str:
     if "AOC_TOKEN" not in os.environ:
-        print("You don't seem to have your aoc session token as \"AOC_TOKEN\" enviromental variable. "
-              "If you want you can input it here to be added in the variable.")
-        token = getpass("Token (from the website): ")
-        os.environ["AOC_TOKEN"] = token
+        print("\nYou don't seem to have your aoc session token as " + Fore.LIGHTBLUE_EX + "\"AOC_TOKEN\"" + Fore.RESET + " enviromental variable. \nIf you want you can input it here to be added in the variable.")
+        token = pwinput("Token (from the website): ")
+        set_key(get_env_file(), "AOC_TOKEN", token)
         return token
     else:
         return os.environ["AOC_TOKEN"]
@@ -16,11 +19,9 @@ def get_token() -> str:
 
 def get_download_path() -> Path:
     if "AOC_DOWNLOAD_PATH" not in os.environ:
-        print("You don't seem to have your download path as \"AOC_DOWNLOAD_PATH\" enviromental variable. "
-              "If you want you can input it here to be added in the variable. "
-              "(If you want you can use ~ as your user folder or vars (such as \"%AppData\" on Windows))")
+        print("\nYou don't seem to have your download path as " + Fore.LIGHTBLUE_EX + "\"AOC_DOWNLOAD_PATH\"" + Fore.RESET + " enviromental variable. \nIf you want you can input it here to be added in the variable. (If you want you can use " + Fore.LIGHTBLUE_EX + "\"~\"" + Fore.RESET + " as your user folder or vars (such as " + Fore.LIGHTBLUE_EX + "\"%AppData%\"" + Fore.RESET + " on Windows))")
         folder = Path(os.path.expandvars(os.path.expanduser(input("Download Path: "))))
-        os.environ["AOC_DOWNLOAD_PATH"] = str(folder)
+        set_key(get_env_file(), "AOC_DOWNLOAD_PATH", str(folder))
         return folder
     else:
         return Path(os.path.expandvars(os.path.expanduser(os.environ["AOC_DOWNLOAD_PATH"])))
@@ -28,11 +29,11 @@ def get_download_path() -> Path:
 
 def get_api_server() -> str:
     if "AOC_API" not in os.environ:
-        return "https://adventofcode.com/"
+        return "https://adventofcode.com"
     else:
-        print("You seem to have the \"AOC_API\" enviromental variable")
+        print("\nYou seem to have the " + Fore.LIGHTBLUE_EX + "\"AOC_API\"" + Fore.RESET + " enviromental variable.")
         selected = input("Do you want to use it as the API Website? (Y or N) ")
-        return os.environ["AOC_API"] if selected == "Y" else "https://adventofcode.com/"
+        return os.environ["AOC_API"] if selected == "Y" else "https://adventofcode.com"
 
 
 def get_cache_path() -> Path:
@@ -40,3 +41,11 @@ def get_cache_path() -> Path:
         return Path(os.getcwd()).joinpath("cache")
     else:
         return Path(os.path.expandvars(os.path.expanduser(os.environ["AOC_CACHE_PATH"])))
+
+
+def get_env_file() -> Path:
+    env_path = find_dotenv()
+    if env_path == "":
+        return Path(os.getcwd()).joinpath(".env")
+    else:
+        return Path(env_path)
